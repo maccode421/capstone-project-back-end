e81f2a000e8e473ce2b8aa53ddd432254efcf87e
maccode421 <maccavalli@yahoo.com>
2017-10-03 19:19:13 -0400

    create vehicle sh folder and auth folder

---
+++ scripts/vehicles/update-vehicle.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+API="${API_ORIGIN:-http://localhost:4741}"
+URL_PATH="/vehicles"
+curl "${API}${URL_PATH}/$ID" \
+  --include \
+  --request PATCH \
+  --header "Content-Type: application/json" \
+  --header "Authorization: Token token=$TOKEN" \
+  --data '{
+    "vehicle": {
+      "user_id": "'"${USER_ID}"'"
+      "make": "'"${MAKE}"'"
+      "model": "'"${MODEL}"'"
+      "year": "'"${YEAR}"'"
+      "mileage": "'"${MILEAGE}"'"
+    }
+  }'
+
+echo

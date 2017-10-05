03f31cd1736778ca54c784c954f7137fa249e6ec
maccode421 <maccavalli@yahoo.com>
2017-10-05 19:45:55 -0400

    modify user has many vehicles, create seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+john = User.create({ email: 'm@m.com', password: 'm' })
+
+vehicles = Vehicle.create({ user_id: john.id, make: 'Ford', model: 'Mustang', year: 1955, mileage: 100000 })
+
+vehicle_logs = VehicleLog.create({ vehicle_id: vehicles.id, mileage: 101000, gas_price: 2.5, repair_type: 'change brakes', total_price: 89.9, date: '7-7-2017', receipt: 'www.go.com' })

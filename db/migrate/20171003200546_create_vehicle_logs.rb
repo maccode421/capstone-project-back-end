9b8727981ccfb743c3b2c82d481251c06795530b
maccode421 <maccavalli@yahoo.com>
2017-10-06 09:18:59 -0400

    fixed car log migration

--- db/migrate/20171003200546_create_vehicle_logs.rb
+++ db/migrate/20171003200546_create_vehicle_logs.rb
@@ -3,9 +3,9 @@ class CreateVehicleLogs < ActiveRecord::Migration[5.0]
     create_table :vehicle_logs do |t|
       t.references :vehicle, foreign_key: true
       t.integer :mileage
-      t.decimal :gas_price, :precision => 8, scale => 2
+      t.decimal :gas_price
       t.string :repair_type
-      t.decimal :total_price, :precision => 8, scale => 2
+      t.decimal :total_price
       t.date :date
       t.string :receipt
 
